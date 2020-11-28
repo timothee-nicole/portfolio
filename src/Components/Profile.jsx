df2d7b606911fdb2ac1cc779b997b66d5daedd90
Timothée Nicole <timothee.nicole@gmail.com>
2020-11-28 18:08:34 +0100

    fixed grammar issues

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -21,11 +21,11 @@ export default function Profile() {
             <br/> Coming from a family of entrepreneurs, I have always wanted to create my own company. Following my graduation for my entrepreneurship master at ESSCA, a French Business  School, I realized that in the current startup ecosystem, a founder needs to have hands-on knowledge of modern web technologies. This is why I decided to join IronHack Bootcamp to learn both frontend and backend web development. During this Bootcamp I became aware of the power and really enjoyed the fun of web programming languages such as JavaScript and am sure I will keep on being impressed as time goes by.</p>
 
             {!display && isMobile ? <p onClick={handleDetails} style={{textAlign: "center", color:"#002395"}}> View More Details...</p> : display ?
-            <><p>A few things about me : I  love spending time with my family and friends, I enjoy going for a swimming sessions between two GitHub push. To me it feels like flying  in the water (Do you know anybody that has never dreamt of flying ?), I like hiking and wandering in the middle of nowhere but also in the middle of the crowd.
+            <><p>A few things about me : I  love spending time with my family and friends, I enjoy going for a swimming sessions between two GitHub push. To me it feels like flying  in the water (do you know anybody that has never dreamt of flying ?), I like hiking and wandering in the middle of nowhere but also in the middle of the crowd.
              </p>
              <ul>
                  <li> 🎮 I like playing video games, I have finished the pokedex. Isn't it a proof of dedication ?  I also like Role Play and Action games. From time to time I also like doing some Sudokus</li>
-                 <li>📚 I also like to reading thrillers, adventure and SF books. Reading enables me to challenge imagination much more than when watching movies</li>
+                 <li>📚 I also like reading thrillers, adventure and SF books. It enables me to challenge imagination much more than when watching movies</li>
                  <li>🎬 I prefer TV Shows to movies. My favorite Movies and TV Shows genres are comedies, SF, actions/adventures, biographies documentaries, not to forget Disney Movies 😍.</li>
              </ul></> : <><p>A few things about me : I  love spending time with my family and friends, I enjoy going for a swimming sessions between two GitHub push. To me it feels like flying  in the water (Do you know anybody that has never dreamt of flying ?), I like hiking and wandering in the middle of nowhere but also in the middle of the crowd.
              </p>
