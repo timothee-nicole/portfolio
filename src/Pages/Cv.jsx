ed03833781ed32a3d4dabb2cdab63787c6b00b77
Timothée Nicole <timothee.nicole@gmail.com>
2020-11-28 18:01:27 +0100

    Second push on heroku

--- src/Pages/Cv.jsx
+++ src/Pages/Cv.jsx
@@ -4,7 +4,7 @@ import '../styles/cv.css'
 export default function Cv() {
     return (
         <div className="cv-container">
-            <object data="./images/resume.pdf" type="application/pdf" width="100%" height="800px"> 
+            <object data="./images/resume.pdf" type="application/pdf" width="100%" height="100%"> 
                 <p>It appears you don't have a PDF plugin for this browser.
                 No biggie... you can <a href="./images/resume.pdf" download>click here to
                 download the PDF file.</a></p>  
